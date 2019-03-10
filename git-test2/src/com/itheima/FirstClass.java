af23312456f379543838f8d69bfe1ba148fbc6ff
孙兴家 <2545241298@qq.com>
2019-03-10 21:19:24 +0800

    着什么玩意

---
+++ git-test2/src/com/itheima/FirstClass.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+/**
+ * @Author: Xingjia Sun
+ * @Description:
+ * @Date: Created in 21:13 2019/3/10
+ * @function
+ * @Modified By:
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
